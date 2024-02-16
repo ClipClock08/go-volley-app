36bc2bf0d3675afbe0db17bcc08e34605717c46c
Oleksandr_Lopatin <o.i.lopatin@student.khai.edu>
2024-02-16 22:23:19 +0200

    only front part

--- go-volley-back-end/internal/models/Season.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Season struct {
-	ID        int    `json:"id"`
-	Title     string `json:"title"`
-	Details   string `json:"details"`
-	StartYear int    `json:"start_year"`
-	EndYear   int    `json:"end_year"`
-}
